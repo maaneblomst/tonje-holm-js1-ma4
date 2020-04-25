019da86b76a02208b1bd5d98ca782d98d9fff350
maaneblomst <60383200+maaneblomst@users.noreply.github.com>
2020-04-25 23:27:54 +0200

    Updated q2

--- question_2/question2.js
+++ question_2/question2.js
@@ -16,9 +16,11 @@ createGames();
 function displayGames(games) {
   const resultsContainer = document.querySelector(".results");
   let html = "";
-  for (object in games.results) {
-    let name = games.results[object].name;
-    let image = games.results[object].background_image;
+  //The next line is not what the example in the lesson said, but after advice from my cousin, I found that using a for/in loop instead made my results visible.
+  //I could not find out how to make it work otherwise.
+  for (detail in games.results) {
+    let name = games.results[detail].name;
+    let image = games.results[detail].background_image;
     html += `<div class="game">
     <h2>${name}</h2>
     <img src="${image}" alt="${name}" width="700" />
